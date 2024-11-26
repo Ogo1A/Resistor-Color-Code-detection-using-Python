06ac1c370b23d29190f6c6fea1d97c1d47d3fea1
Darren Ogola <darren.sunday@strathmore.edu>
2024-11-26 23:19:05 +0300

    My Day32 of 365 challange

---
+++ Day32.py
@@ -0,0 +1,9 @@
+color=["black","brown","red","orange","yellow","green","blue","violet","grey","white"]
+Resistor Color Code detection using Python
+n=color. index((input("Enter the 1st color :"))) m=color.index((input("Enter the 2nd color :")))
+p=color.index((input("Enter the 3rd color :")))
+q=int(((n*10)+(m))*(10**(p)))
+z=q/1000
+#clcoding.com
+print("\nThe Resister Value Is :")
+print(f"{q}0 and in Kiloohm :
